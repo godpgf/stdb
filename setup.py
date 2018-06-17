e33bf16c0f7ef04229a077016c25b3d4d0e8ea27
严宇 <yuyan@jieyuechina.com>
2018-06-17 14:43:07 +0800

    continue

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError: # for pip <= 9.0.3
 
 setup(
     name='stdb',
-    version='0.0.1',
+    version='0.0.2',
     description='Stock Data Engine',
     packages=find_packages(exclude=[]),
     author='godpgf',
